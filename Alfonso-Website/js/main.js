94884d39b11b42682515bd06ccac2b46fd5d315f
Denis Alfonso <cubanyeti@gmail.com>
2018-11-23 11:10:49 -0500

    v1.2
    Finally fixed shrinking nav to also shrink the anchor links.

--- Alfonso-Website/js/main.js
+++ Alfonso-Website/js/main.js
@@ -1,16 +1,30 @@
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
+    
+    var anchorFont = document.querySelectorAll(".move");
+    
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("primary_nav_wrap").style.height = "100px";
     document.getElementById("mylogo").style.width = "75px";
       
-    document.getElementById("primary_nav_wrap ul li").style.marginTop = "80px";
-    document.getElementById("primary_nav_wrap ul").style.fontSize = "10px";
+    document.querySelector("#primary_nav_wrap ul").style.marginTop = "5px";
     
-  } else {
+      
+      for( i in anchorFont ) {
+        anchorFont[i].style.fontSize = "12px";
+      }
+      
+    } else {
+        
     document.getElementById("primary_nav_wrap").style.height = "150px";
     document.getElementById("mylogo").style.width = "125px";
-    document.getElementById("primary_nav_wrap ul li").style.marginTop = "50px";
+    document.querySelector("#primary_nav_wrap ul").style.marginTop = "37px";
+    
+        for( i in anchorFont ) {
+        anchorFont[i].style.fontSize = "20px";
+      }
+      
+     
   }
 }
