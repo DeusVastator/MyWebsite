8fcd4c6ee3b1511b2f9e325399d1ec192ca60cef
Denis Alfonso <cubanyeti@gmail.com>
2018-11-22 22:51:39 -0500

    v1.12
    Fixed up some of the page height so shrinking nav doesnt cover content. Attempted to fix nav links but still unable. Temporarily changed position to look better when shrinking but the links themselves do not shrink on scroll.

--- Alfonso-Website/js/main.js
+++ Alfonso-Website/js/main.js
@@ -5,12 +5,12 @@ function scrollFunction() {
     document.getElementById("primary_nav_wrap").style.height = "100px";
     document.getElementById("mylogo").style.width = "75px";
       
-    document.querySelector("#primary_nav_wrap ul li").style.marginTop = "40px";
-    document.querySelector("#nav a").style.fontSize = "15px";
+    document.getElementById("primary_nav_wrap ul li").style.marginTop = "80px";
+    document.getElementById("primary_nav_wrap ul").style.fontSize = "10px";
+    
   } else {
     document.getElementById("primary_nav_wrap").style.height = "150px";
     document.getElementById("mylogo").style.width = "125px";
-    document.querySelector("#primary_nav_wrap ul li").style.marginTop = "80px";
-    document.querySelector("#nav a").style.fontSize = "20px";
+    document.getElementById("primary_nav_wrap ul li").style.marginTop = "50px";
   }
 }
